a0c8b2de4600983fac8f64b5724a5342e94e9c5c
hackgt@node13.info <hackgt@hackgt.com>
2016-05-25 13:05:56 -0400

    Changin file names

--- github.sh
+++
@@ -1,2 +0,0 @@
-git --git-dir=/home/hackgt/web/sponsors.git --work-tree=/home/hackgt/web/sponsors fetch
-git --git-dir=/home/hackgt/web/sponsors.git --work-tree=/home/hackgt/web/sponsors merge origin/master
